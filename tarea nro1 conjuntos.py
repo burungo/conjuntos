45609686e17bef3afb21fbe5472f80c71658e881
burungo <calabreado@gmail.com>
2025-04-10 23:34:48 -0300

    Add files via upload

---
+++ tarea nro1 conjuntos.py
@@ -0,0 +1,42 @@
+#Código para la Unión de Conjuntos
+
+#A = {1, 2, 3, 4, 5}
+#B = {4, 5, 6, 7, 8}
+
+#union = A.union(B)
+
+#print("La unión de los conjuntos A y B es:", union)
+
+#Código para la Intersección de Conjuntos.
+
+#A = {1, 2, 3, 4, 5}
+#B = {4, 5, 6, 7, 8}
+
+#interseccion = A & B
+
+#print("La intersección de los conjuntos A y B es:", interseccion)
+
+#Código para la Diferencia de Conjuntos
+
+#A = {1, 2, 3, 4, 5}
+#B = {4, 5, 6, 7, 8}
+
+#diferencia_simétrica = A ^ B
+
+#print("La diferencia simétrica de los conjuntos A y B es:", diferencia_simétrica)
+
+#Código para Verificar si un Conjunto es un Subconjunto de Otro 
+
+#A = {1, 2, 3}
+#B = {1, 2, 3, 4, 5}
+
+#if A.issubset(B):
+    #print("El conjunto A es un subconjunto de B")
+#else:
+    #print("El conjunto A no es un subconjunto de B")
+
+#Código para Imprimir el Número de Elementos de un Conjunto en Python
+
+A = {1, 2, 3, 4, 5}
+
+print("El número de elementos del conjunto A es:", len(A))
